e0d569eb679b69930e5e524d54183883ce8ba275
Ericlys Moreira <ericlysm@gmail.com>
2022-04-04 13:50:04 -0300

    configurando setup de autenticacao do purchases

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// process.env
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+})
+export class HttpModule {}
