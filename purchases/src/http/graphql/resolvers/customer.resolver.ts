3f4bcca1c9542f24cdfed81f629653ccaa1b7558
Ericlys Moreira <ericlysm@gmail.com>
2022-12-09 17:42:07 -0300

    feat: consumer buy list

---
+++ purchases/src/http/graphql/resolvers/customer.resolver.ts
@@ -0,0 +1,26 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { Customer } from '../model/customer';
+import { CustomersService } from '../../../services/customers.service';
+import { AuthUser, CurrentUser } from '../../auth/current-user';
+import { PurchasesService } from '../../../services/purchases.service';
+
+@Resolver(() => Customer)
+export class CustomersResolver {
+  constructor(
+    private customerService: CustomersService,
+    private purchasesService: PurchasesService,
+  ) {}
+
+  @UseGuards(AuthorizationGuard)
+  @Query(() => Customer)
+  me(@CurrentUser() user: AuthUser) {
+    return this.customerService.getCustomerByAuthUserId(user.sub);
+  }
+
+  @ResolveField()
+  purchases(@Parent() customer: Customer) {
+    return this.purchasesService.listAllFromCustomer(customer.id);
+  }
+}
